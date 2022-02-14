2c8386b002fd41f4103155b895126231db718b6f
coder <bizdb@kakao.com>
2022-02-14 14:40:32 +0900

    2022.2.14(14:40)
    2022.2.14(14:40)

--- src/common/SEO.js
+++ src/common/SEO.js
@@ -6,7 +6,7 @@ const SEO = ( {title} ) => {
     return (
         <>
             <meta charSet="utf-8" />
-            <title>{title} || Foreigner VISA 외국인 비자 상담 </title>
+            <title>{title} || korea VISA 한국 비자</title>
             <meta name="robots" content="noindex, follow" />
             <meta name="description" content="korea Foreign visa job Consulting" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
--- src/pages/inquiry/Workinquiry.jsx
+++ src/pages/inquiry/Workinquiry.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import SEO from "../../common/SEO";
 import Layout from "../../common/Layout";
 import BreadcrumbOne from "../../elements/breadcrumb/BreadcrumbOne";
+import { FiArrowRight , FiPlay } from "react-icons/fi";
 
+import ScrollAnimation from "react-animate-on-scroll";
 import SectionTitle from "../../elements/sectionTitle/SectionTitle";
 import AccordionJob from "../../elements/accordion/AccordionJob";
 import Separator from "../../elements/separator/Separator";
@@ -20,11 +22,25 @@ const Workinquiry = () => {
                     currentUrl="외국인 취업 희망자에게 일자리를 안내해드립니다."
                 />
                 <div className="main-content">
-
-
                     {/* Start Accordion Area  */}
                     <div className="rn-accordion-area rn-section-gap">
                         <div className="container">
+                            <ScrollAnimation
+                                animateIn="fadeInUp"
+                                animateOut="fadeInOut"
+                                animateOnce={true}
+                                className="button-group mb--50" style={{
+                                    display:"flex",
+                                    justifyContent:"center",
+                                    flexDirection:"column",
+                                    textAlign:"center"
+                                }}>
+                                    <div>혹시 기업이신가요?</div>
+                                    <div>외국인을 채용하고 싶으시다면 아래 버튼을 눌러주세요</div>
+                                <a target="_blank" href="https://thekvisa.kr" rel="noopener noreferrer">
+                                    <button className="btn-default btn-large btn-icon" to="#">외국인 채용 공고 등록<i className="icon"><FiArrowRight /></i></button>
+                                </a>
+                            </ScrollAnimation>
                             <div className="row">
                                 <div className="col-lg-10 offset-lg-1">
                                     <SectionTitle
