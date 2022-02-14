bb6406695708d4a9b0f2c28ad50bf94db16d31e3
coder <bizdb@kakao.com>
2022-02-14 15:28:35 +0900

    2022.2.14(15:30)
    2022.2.14(15:30)

--- src/common/SEO.js
+++ src/common/SEO.js
@@ -7,7 +7,7 @@ const SEO = ( {title} ) => {
         <>
             <meta charSet="utf-8" />
             <title>{title} || korea VISA 한국 비자</title>
-            <meta name="robots" content="noindex, follow" />
+            <meta name="robots" content="index, follow" />
             <meta name="description" content="korea Foreign visa job Consulting" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         </>
